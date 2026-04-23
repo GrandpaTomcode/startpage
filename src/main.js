7c367c2d8e72e80ae6a674e8a34ee2dee2e9f190
Grandpatom <tomhornbuckle1@gmail.com>
2026-04-24 00:39:57 +0100

    Search clears input on enter and code clean

--- src/main.js
+++ src/main.js
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const greetingText = document.getElementById('greeting');
-    var date = new Date();
-    var time = date.getHours();
+document.addEventListener("DOMContentLoaded", () => {
+  const greetingText = document.getElementById("greeting");
+  
+  var getCurrentGreeting = () => {
+    const currentHour = new Date().getHours();
 
-    var getCurrentGreeting = () => {
-        const currentHour = new Date().getHours();
-
-        if (currentHour >= 5 && currentHour < 12) {
-            return 'Good Morning, Tom!';
-        } else if (currentHour >= 12 && currentHour < 18) {
-            return 'Good Afternoon, Tom!';
-        } else {
-            return 'Good Evening, Tom!';
-        }
+    if (currentHour >= 5 && currentHour < 12) {
+      return "Good Morning, Tom!";
+    } else if (currentHour >= 12 && currentHour < 18) {
+      return "Good Afternoon, Tom!";
+    } else {
+      return "Good Evening, Tom!";
     }
+  };
 
-    var updateGreeting = () => {
-        const greeting = getCurrentGreeting();
-        greetingText.textContent = greeting;
-    }
+  var updateGreeting = () => {
+    const greeting = getCurrentGreeting();
+    greetingText.textContent = greeting;
+  };
 
-    updateGreeting();
-    setInterval(updateGreeting, 60000);
+  updateGreeting();
+  setInterval(updateGreeting, 60000);
 
+  const searchForm = document.querySelector(".search");
+  searchForm.addEventListener("submit", () => {
+    searchForm.reset();
+  });
 });
-
